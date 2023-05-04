a29f45f899679383c123efdc7c3c3871a9f26cba
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2023-05-05 00:01:15 +0600

    done

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:photo_editor_pro/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Image Editor',
+      theme: ThemeData.light(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
